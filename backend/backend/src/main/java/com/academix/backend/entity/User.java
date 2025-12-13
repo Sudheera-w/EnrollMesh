b1b3e54a9e205b59901361f2b071db5829809630
Sudheera Weerawardhana <stweerawardhana@gmail.com>
2025-12-13 22:59:47 +0530

    feat(user): Add user entity

---
+++ backend/backend/src/main/java/com/academix/backend/entity/User.java
@@ -0,0 +1,26 @@
+package com.academix.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role;  // 2 Roles -> Admin/student
+}
