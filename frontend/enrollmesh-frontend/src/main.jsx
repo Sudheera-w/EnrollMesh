fa1116c468c85e320fa6a63afcc41e68cd9a7fba
Sudheera Weerawardhana <stweerawardhana@gmail.com>
2025-12-22 23:50:40 +0530

    changed main.jsx for deployment

--- frontend/enrollmesh-frontend/src/main.jsx
+++ frontend/enrollmesh-frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import StudentService from "./services/StudentService";
+import StudentService from "./services/StudentService.jsx";
 
 window.StudentService = StudentService;
 createRoot(document.getElementById('root')).render(
